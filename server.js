7356a5edebb94ca084a10d15c65cc58985628058
Rachel Fritz <rachelfritz0@gmail.com>
2020-08-10 14:57:19 -0500

    installed heroku and added to server

--- server.js
+++ server.js
@@ -42,12 +42,26 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
-    let results = animals;
+    let result = animals;
     if (req.query) {
-        results = filterByQuery(req.query, results);
+        results = filterByQuery(req.query, result);
     }
-    res.json(animals);
+    res.json(result);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
